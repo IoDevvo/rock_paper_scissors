5ca288f70e0d53342c2799d6ecbdd56f991bec5d
angelo <angelogabrieledm@gmail.com>
2022-09-04 15:18:44 +0200

    Add a function for single round I used a series of if functions to describe a single round of rock, paper and scissors. Also, a win or loss increases the respective variable. This will come in handy when a final score will be returned.

--- rps.js
+++ rps.js
@@ -7,3 +7,41 @@ function getComputerChoice(){
     return choice;
 }
 
+function playRound(playerSelection, computerSelection){
+    console.log(computerSelection);
+    if(playerSelection == "rock"){
+        if(computerSelection == "rock"){
+            return "It's a Draw!";
+        } else if(computerSelection == "paper"){
+            lose++;
+            return "Paper beats rock, you lose!";
+        } else if(computerSelection == "scissors"){
+            win++;
+            return "Rock beats scissors, you win!";
+        }
+    }
+    if(playerSelection == "paper"){
+        if(computerSelection == "rock"){
+            win++;
+            return "Paper beats rock, you win!";
+        } else if(computerSelection == "paper"){
+            return "It's a draw!";
+        } else if(computerSelection == "scissors"){
+            lose++;
+            return "Scissors beats paper, you lose!";
+        }
+    }
+    if(playerSelection == "scissors"){
+        if(computerSelection == "rock"){
+            lose++;
+            return "Rock beats scissors, you lose!";
+        } else if(computerSelection == "paper"){
+            win++;
+            return "Scissors beats paper, you win!";
+        } else if(computerSelection == "scissors"){
+            return "It's a draw!";
+        }
+    } else{
+        return "Invalid choice";
+    }
+}

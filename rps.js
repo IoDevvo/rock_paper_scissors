b1b1b59e9c712368cfd0cda0554a6de8bdeb2cab
angelo <angelogabrieledm@gmail.com>
2022-09-04 15:43:43 +0200

    Add loop for five rounds First I needed to get an input from the user, to use it as an argument in the playRound function. The result is shown in the console.

--- rps.js
+++ rps.js
@@ -1,11 +1,12 @@
-let win = 0;
-let lose = 0;
+let win = 0; 
+let lose = 0;//these two variables are forteh final score
 const items = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     let choice = items[Math.floor(Math.random() * items.length)];
     return choice;
-}
+} //this function returns a random value in the array, later will assign this function to the variable computerSelection
+
 
 function playRound(playerSelection, computerSelection){
     console.log(computerSelection);
@@ -44,4 +45,15 @@ function playRound(playerSelection, computerSelection){
     } else{
         return "Invalid choice";
     }
+}//this code plays a round of the game and returns the values
+
+function game(){
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Pick Rock, Paper or Scissors:", "");
+        playerSelection = playerSelection.toLowerCase();
+        let computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }//this loop first asks for a prompt by the user, then makes it case insensitive with the toLowerCase() method. Then plays the round for five times
 }
+
+game();
